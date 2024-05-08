5f1c18e0d5e3a9dde56911f45301010d590b9584
Federico Carranza <fcarranz@student.42barcelona.com>
2024-05-08 14:23:15 +0200

    Cambio variables por una estructura para poder pasarla entre funciones auxiliares para cerrar file descriptors o liberar punteros

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 09:47:52 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/05/06 10:21:49 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/05/08 13:49:42 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 #include "get_next_line.h"
 #include <stdio.h>
 #include <unistd.h>
+
+typedef struct s_pipex
+{
+	char	**cmd1;
+	char	**cmd2;
+	char	*path1;
+	char	*path2;
+	int		fd1;
+	int		fd2;
+	int		fds[2];
+}	t_pipex;
 
 char	*ft_get_path(char **env, char *cmd);
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 09:47:26 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/05/06 14:02:18 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/05/08 13:58:24 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,30 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	int		fd1;
-	int		fd2;
-	int		fds[2];
-	char	**cmd1;
-	char	**cmd2;
-	char	*path1;
-	char	*path2;
+	t_pipex	pipex;
 	int		pid;
 
 	if (argc != 5)
 		return (perror("INVALID NUMBER OF ARGUMENTS\n"), 1);
-	fd1 = open(argv[1], O_RDONLY);
-	fd2 = open(argv[4], O_WRONLY | O_CREAT, 0777);
+	pipex.fd1 = open(argv[1], O_RDONLY);
+	pipex.fd2 = open(argv[4], O_WRONLY | O_CREAT, 0777);
 
-	cmd1 = ft_split(argv[2], ' ');
-	cmd2 = ft_split(argv[3], ' ');
+	pipex.cmd1 = ft_split(argv[2], ' ');
+	pipex.cmd2 = ft_split(argv[3], ' ');
 
-	if (fd1 == -1)
+	if (pipex.fd1 == -1)
 		return (ft_printf("no such file or directory: %s\n", argv[1]), 1);
-	fd2 = open(argv[4], O_RDONLY);
-	if (fd2 == -1)
+	if (pipex.fd2 == -1)
 		return (ft_printf("no such file or directory: %s\n", argv[4]), 1);
 
-	path1 = ft_get_path(env, cmd1[0]);
-	if (!path1)
-		return (ft_printf("command not found: %s\n", cmd1[0]), 1);
-	path2 = ft_get_path(env, cmd2[0]);
-	if (!path2)
-		return (free (path1), ft_printf("command not found: %s\n", cmd2[0]), 1);
+	pipex.path1 = ft_get_path(env, pipex.cmd1[0]);
+	if (!pipex.path1)
+		return (ft_printf("command not found: %s\n", pipex.cmd1[0]), 1);
+	pipex.path2 = ft_get_path(env, pipex.cmd2[0]);
+	if (!pipex.path2)
+		return (free (pipex.path1), ft_printf("command not found: %s\n", pipex.cmd2[0]), 1);
 
-	if (pipe(fds) == -1)
+	if (pipe(pipex.fds) == -1)
 	{
 		perror("Error pipe()\n");
 		exit(EXIT_FAILURE);
@@ -57,10 +50,11 @@ int	main(int argc, char **argv, char **env)
 	}
 	if (pid == 0)
 	{
-		close(fds[0]);
-		dup2(fds[1], STDOUT_FILENO);
-		close(fds[1]);
-		execve(path1, cmd1, NULL);
+		close(pipex.fds[0]);
+		dup2(pipex.fd1, STDIN_FILENO);
+		dup2(pipex.fds[1], STDOUT_FILENO);
+		close(pipex.fds[1]);
+		execve(pipex.path1, pipex.cmd1, NULL);
 	}
 	pid = fork();
 	if (pid == -1)
@@ -70,18 +64,18 @@ int	main(int argc, char **argv, char **env)
 	}
 	if (pid == 0)
 	{
-		close(fds[1]);
-		dup2(fds[0], STDIN_FILENO);
-		close(fds[1]);
-		dup2(fd2, STDOUT_FILENO);
-		close(fd2);
-		execve(path2, cmd2, NULL);
+		close(pipex.fds[1]);
+		dup2(pipex.fds[0], STDIN_FILENO);
+		close(pipex.fds[1]);
+		dup2(pipex.fd2, STDOUT_FILENO);
+		close(pipex.fd2);
+		execve(pipex.path2, pipex.cmd2, NULL);
 	}
-	close(fds[0]);
-	close(fds[1]);
+	close(pipex.fds[0]);
+	close(pipex.fds[1]);
 	wait(NULL);
-	ft_free_array((void *)cmd1);
-	ft_free_array((void *)cmd2);
-	ft_printf("%s esta en %s y %s en %s\n", argv[2], path1, argv[3], path2);
-	return (free (path1), free (path2), 0);	
+	ft_free_array((void *)pipex.cmd1);
+	ft_free_array((void *)pipex.cmd2);
+	ft_printf("%s esta en %s y %s en %s\n", argv[2], pipex.path1, argv[3], pipex.path2);
+	return (free (pipex.path1), free (pipex.path2), 0);	
 }

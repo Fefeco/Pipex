24f5c26845aa946ceaef37e72c586e5a8f2502e1
Federico Carranza <fcarranz@student.42barcelona.com>
2024-05-16 13:56:28 +0200

    Agrego mensajes de error por permisos, ausencia de archivo o error en la funcion open()

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,22 +6,37 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:50:38 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/05/16 12:09:02 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/05/16 13:31:08 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+static void	ft_open_files_aux(char *file_name, char *msj)
+{
+	ft_putstr_fd(msj, 2);
+	ft_putstr_fd(file_name, 2);
+	write(2, "\n", 1);
+}
+
 int	ft_open_files(int argc, char **argv, t_pipex *pipex)
 {
 	int	i;
 
+	if (access(argv[1], F_OK))
+	{
+		ft_open_files_aux(argv[1], "pipex: no such file or directory: ");
+		return (1);
+	}
+	if (access(argv[1], R_OK))
+	{
+		ft_open_files_aux(argv[1], "pipex: permission denied: ");
+		return (1);
+	}
 	pipex->fd_in = open(argv[1], O_RDONLY);
 	if (pipex->fd_in == -1)
 	{
-		ft_putstr_fd("no such file or directory: ", 2);
-		ft_putstr_fd(argv[1], 2);
-		write(2, "\n", 1);
+		ft_open_files_aux(argv[1], "pipex: error open file: ");
 		return (1);
 	}
 	i = 0;

ef9de306b9253bed175640ac339a0f1f2b98556a
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-06-12 13:39:46 +0200

    Elimino el malloc() en la funcion y utilizo el get_next_line() para inicializar la variable que antes lo hacia con el malloc()

--- src/read.c
+++ src/read.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 11:59:40 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/05/20 12:27:14 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/06/12 13:38:39 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_read_from_file(int fd)
 	char	*read_line;
 	char	*full_line;
 
-	full_line = (char *)malloc(1);
+	full_line = get_next_line(fd);
 	if (!full_line)
-		return (full_line);
+		return (NULL);
 	read_line = get_next_line(fd);
 	while (read_line)
 	{

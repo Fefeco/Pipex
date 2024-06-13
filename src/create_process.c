6fb4846e90d87125060e14e72a81bffddc2c0766
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-06-13 18:33:00 +0200

    Cambios en la creacion de los file descriptors. Nueva version de ft_init_fds()

--- src/create_process.c
+++ src/create_process.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 19:39:38 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/05/21 14:26:59 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/06/13 17:44:30 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	ft_create_process(t_pipex *pipex, int i)
 {
-	pipex->pid[i] = fork();
-	if (pipex->pid[i] == -1)
+	int	pid;
+
+	pid = fork();
+	if (pid == -1)
 		return (1);
-	if (pipex->pid[i] == 0)
+	if (pid == 0)
 	{
 		dup2 (pipex->fds[i][0], STDIN_FILENO);
 		if (i < pipex->cmd_len - 1)
@@ -28,6 +30,6 @@ int	ft_create_process(t_pipex *pipex, int i)
 		execve (pipex->path[i], pipex->cmd[i], NULL);
 	}
 	close (pipex->fds[i][1]);
-	waitpid(pipex->pid[i], NULL, 0);
+	waitpid(pid, NULL, 0);
 	return (0);
 }
--- src/init.c
+++ src/init.c
@@ -6,71 +6,32 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 20:31:07 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/06/12 18:58:55 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/06/13 18:27:29 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_init_fds(t_pipex *pipex)
+int	**ft_init_fds(int len)
 {
 	int	i;
-	int	len;
+	int	**fds;
 
-	len = pipex->cmd_len;
-	pipex->fds = (int **)malloc(sizeof(int **) * (len));
-	if (!pipex->fds)
-	{
-		ft_free_cmds(pipex);
-		ft_free_array((void **)pipex->path);
-		exit(EXIT_FAILURE);
-	}
+	fds = (int **)malloc(sizeof(int *) * len - 1);
+	if (!fds)
+		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len - 1)
 	{
-		pipex->fds[i] = (int *)malloc(sizeof(int) * 2);
-		if (!pipex->fds[i])
+		fds[i] = (int *)malloc(sizeof(int) * 2);
+		if (!fds[i])
 		{
-			ft_free_fds(pipex);
-			exit(EXIT_FAILURE);
+			while (--i >= 0)
+				free(fds[i]);
+			free (fds);
+			return (NULL);
 		}
 		++i;
 	}
-}
-
-void	ft_init_pids(t_pipex *pipex)
-{
-	int	len;
-
-	len = pipex->cmd_len;
-	pipex->pid = (int *)malloc(sizeof(int *) * len);
-	if (!pipex->pid)
-	{
-		ft_free_cmds(pipex);
-		ft_free_array((void **)pipex->path);
-		ft_free_fds(pipex);
-		exit(EXIT_FAILURE);
-	}
-}
-
-t_cmd	*ft_init_cmd(char *arg, char **env)
-{
-	t_cmd	*cmd;
-
-	cmd = (t_cmd *)malloc(sizeof(t_cmd));
-	if (!cmd)
-		exit(EXIT_FAILURE);
-	cmd->next = NULL;
-	cmd->args = ft_split(arg, ' ');
-	if (!cmd->args)
-	{
-		free (cmd);
-		perror("Fail ft_split()");
-		exit(EXIT_FAILURE);
-	}
-	if (cmd->args[0][0] == '/')
-		cmd->path = cmd->args[0];
-	else
-		cmd->path = ft_get_path(env, cmd->args[0]);
-	return (cmd);
+	return (fds);
 }

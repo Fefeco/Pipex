57b5e0068fec2a9b717b8c36564f742dfd8d36e3
Fefeco <carranzafederico@gmail.com>
2024-06-14 13:11:10 +0200

    Refactorizo la funcion ft_create_process() donde chequeo que los argumentos que necesito para el proceso hijo y en cas de ok llamo a nuueva funcion ft_exec_process() donde hago el fork()

--- src/create_process.c
+++ src/create_process.c
@@ -6,55 +6,48 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 19:39:38 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/06/13 20:54:47 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:41:59 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	ft_first_cmd(char *file_name)
+static void	ft_exec_process(t_pipex *pipex, int i)
 {
-	int	fd_in;
-
-	fd_in = ft_open_fd(file_name, O_RDONLY);
-	if (fd_in == -1)
-		return (1);
-	dup2 (fd_in, STDIN_FILENO);
-	return (0);
+	if (pipex->cmds->index == 0)
+		dup2 (pipex->fd_in, STDIN_FILENO);
+	else
+		dup2 (pipex->fds[i - 1][0], STDIN_FILENO);
+	if (!pipex->cmds->next)
+		dup2 (pipex->fd_out, STDOUT_FILENO);
+	else
+		dup2 (pipex->fds[i][1], STDOUT_FILENO);
+	execve (pipex->cmds->path, pipex->cmds->command, NULL);
+	perror ("pipex");
 }
 
-int	ft_last_cmd(char *file_name)
-{
-	int	fd_out;
-
-	fd_out = ft_open_fd(file_name, O_WRONLY | O_TRUNC | O_CREAT);
-	if (fd_out == -1)
-		return (1);
-	dup2 (fd_out, STDOUT_FILENO);
-	return (0);
-}
-
-int	ft_create_process(char **argv, t_pipex *pipex, int i)
+int	ft_create_process(t_pipex *pipex, int i)
 {
 	int	pid;
 
+	if (pipex->cmds->index == 0 && pipex->fd_in == -1)
+		return (close (pipex->fds[i][1]));
+	if (!pipex->cmds->next && pipex->fd_out == -1)
+		return (1);
+	if (!pipex->cmds->path)
+	{
+		ft_print_stderr(pipex->cmds->command[0], "command not found");
+		if (i < pipex->tot_cmds - 1)
+			close (pipex->fds[i][1]);
+		return (1);
+	}
 	pid = fork();
 	if (pid == -1)
 		return (1);
 	if (pid == 0)
-	{
-		if (i == 0 && ft_first_cmd(argv[0]))
-			return (close (pipex->fds[i][1]));
-		else
-			dup2 (pipex->fds[i - 1][0], STDIN_FILENO);
-		if (!pipex->cmds->next && ft_last_cmd(argv[pipex->tot_cmds + 1]))
-			return (close (pipex->fds[i][1]));
-		else
-			dup2 (pipex->fds[i][1], STDOUT_FILENO);
-		execve (pipex->cmds->path, pipex->cmds->command, NULL);
-		perror ("pipex");
-	}
-	close (pipex->fds[i][1]);
+		ft_exec_process(pipex, i);
+	if (i < pipex->tot_cmds - 1)
+		close (pipex->fds[i][1]);
 	waitpid(pid, NULL, 0);
 	return (0);
 }

a957eab1d69de9517b9d7364346fa630386c28e8
Fefeco <carranzafederico@gmail.com>
2024-06-14 13:05:53 +0200

    Impllemento funciones ft_free_cmd_strct() y ft_print_stderr(). Reimplemento las funciones ft_open_fd_in() y  ft_open_fd_out() pero simplificadas

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,24 +6,51 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:50:38 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/06/13 21:01:48 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:59:21 by fedeito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	ft_open_fd(char *file_name, int mode)
+void	ft_free_cmd_struct(t_cmd *cmd)
+{
+	int	i;
+
+	if (cmd->path)
+		free (cmd->path);
+	i = 0;
+	while (cmd->command[i])
+		free (cmd->command[i++]);
+	free (cmd->command);
+	free (cmd);
+	cmd = NULL;
+}
+
+void	ft_print_stderr(char *cause, char *error)
+{
+	write(2, "pipex: ", 7);
+	write(2, cause, ft_strlen(cause));
+	write(2, ": ", 2);
+	write(2, error, ft_strlen(error));
+	write(2, "\n", 1);
+}
+
+int	ft_open_fd_in(char *file_name, int mode)
 {
 	int		fd;
 
-	fd = -1;
-	if (access(file_name, F_OK | R_OK))
-		ft_printf("pipex: %s: %s\n", strerror(errno), file_name);
-	else
-	{
-		fd = open(file_name, mode);
-		if (fd == -1)
-			ft_printf("pipex: %s\n", strerror(errno));
-	}
+	fd = open(file_name, mode);
+	if (fd == -1)
+		ft_printf("pipex: %s: %s\n", file_name, strerror(errno));
+	return (fd);
+}
+
+int	ft_open_fd_out(char *file_name, int mode)
+{
+	int		fd;
+
+	fd = open(file_name, mode, 0664);
+	if (fd == -1)
+		ft_printf("pipex: %s: %s\n", file_name, strerror(errno));
 	return (fd);
 }

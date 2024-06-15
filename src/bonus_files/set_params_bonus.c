49f308e0a5f1359bd703a6f184bdbd9dd22f27ab
Fefeco <carranzafederico@gmail.com>
2024-06-15 22:19:20 +0200

    FIX BUG. En la funcion ft_set_params() faltaba salir de la funcion si es que entraba en la condicion

--- src/bonus_files/set_params_bonus.c
+++ src/bonus_files/set_params_bonus.c
@@ -17,17 +17,18 @@ void	ft_set_params(t_pipex *pipex, char **argv, int argc, char **env)
 	char	*read_from_here_doc;
 	int		fd;
 
-	if (pipex->here_doc_exist)
+	if (!pipex->here_doc_exist)
 	{
-		read_from_here_doc = ft_read_from(STDIN_FILENO, argv[2]);
-		fd = open(pipex->hd_file, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-		write (fd, read_from_here_doc, ft_strlen(read_from_here_doc));
-		close (fd);
-		free (read_from_here_doc);
-		read_from_here_doc = NULL;
-		pipex->tot_cmds = argc - 4;
-		pipex->cmds = ft_parser(argv + 3, pipex->tot_cmds, env);
+		pipex->tot_cmds = argc - 3;
+		pipex->cmds = ft_parser(argv + 2, pipex->tot_cmds, env);		
+		return ;
 	}
-	pipex->tot_cmds = argc - 3;
-	pipex->cmds = ft_parser(argv + 2, pipex->tot_cmds, env);
+	read_from_here_doc = ft_read_from(STDIN_FILENO, argv[2]);
+	fd = open(pipex->hd_file, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	write (fd, read_from_here_doc, ft_strlen(read_from_here_doc));
+	close (fd);
+	free (read_from_here_doc);
+	read_from_here_doc = NULL;
+	pipex->tot_cmds = argc - 4;
+	pipex->cmds = ft_parser(argv + 3, pipex->tot_cmds, env);
 }

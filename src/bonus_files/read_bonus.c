328b72ee238c33faba32f4249f3bec3b8d11caaa
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-06-16 15:05:52 +0200

    FIX_BUG. Cambios en la funcion que lee desde el here_doc porque paraba antes si matcheaba con los primeros caracteres

--- src/bonus_files/read_bonus.c
+++ src/bonus_files/read_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:01:10 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/06/16 10:47:51 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/06/16 15:03:28 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ char	*ft_read_from(int fd, char *stop)
 
 	full_line = NULL;
 	read_line = get_next_line(fd);
+	if (stop)
+		stop = ft_strjoin(ft_strdup(stop), ft_strdup("\n"));
 	while (read_line)
 	{
-		if (stop && !ft_strncmp_prot(read_line, stop, ft_lenstr(read_line) - 1))
+		if (stop && !ft_strncmp_prot(read_line, stop, ft_lenstr(stop)))
 		{
 			free (read_line);
+			free (stop);
 			break ;
 		}
 		if (!full_line)
